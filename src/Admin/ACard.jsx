0a89f5cfff2c1b4826850d6c4bdb1243ae75f715
umargafia <77011124+umargafia@users.noreply.github.com>
2022-07-15 17:32:59 +0100

    add loading

--- src/Admin/ACard.jsx
+++ src/Admin/ACard.jsx
@@ -6,7 +6,6 @@ import {
   CardActions,
   CardContent,
   CardHeader,
-  Typography,
   IconButton,
 } from "@material-ui/core";
 import {
@@ -15,7 +14,7 @@ import {
   DeleteOutline,
   DeleteSharp,
 } from "@mui/icons-material";
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -39,63 +38,82 @@ const useStyles = makeStyles({
 });
 function ACard() {
   const classes = useStyles();
-
+  const [ispending, setpending] = useState(true);
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:8000/cart")
       .then((res) => res.json())
-      .then((data) => setCart(data));
+      .then((data) => {
+        setCart(data);
+        setpending(false);
+      });
   }, []);
 
   const handleDeleate = async (id) => {
-    await fetch(`http://localhost:8000/cart/${id}`, {
+    await fetch(`http://localhost:8000/cart/{id}`, {
       method: "DELETE",
     });
     const newCart = cart.filter((cart) => cart.id != id);
-    console.log(newCart);
     setCart(newCart);
   };
 
-  return cart.map((carts) => (
-    <Grid item md={3} key={carts.id}>
-      <Card className={classes.root}>
-        <CardHeader
-          avatar={<Avatar className={classes.avatar}>C</Avatar>}
-          title={carts.name}
-          action={
-            <IconButton onClick={() => handleDeleate(carts.id)}>
-              <DeleteOutline />
-            </IconButton>
-          }
-        />
-        <CardContent>
-          <Typography component="i">From</Typography>
-          <Typography variant="h4">
-            {carts.start} <ArrowRightAlt /> {carts.stop}
+  return (
+    <>
+      {ispending && (
+        <div
+          style={{
+            width: "100%",
+          }}
+        >
+          <Typography variant="h1" color="black" textAlign={"center"}>
+            Loading...
           </Typography>
-          <Typography color="primary">{carts.date}</Typography>
-          <Typography>{carts.time} Hours</Typography>
-          <Typography className={classes.priceName}>₦{carts.price}</Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" className={classes.bookNowButton}>
-            Update
-          </Button>
-          {/* <Button size="small" className={classes.deleteButton}>
-            Delete
-          </Button> */}
-
-          <Link to={"/vieworder"}>
-            <Button size="small">
-              orders 10
-              <ArrowRightSharp />
-            </Button>
-          </Link>
-        </CardActions>
-      </Card>
-    </Grid>
-  ));
+        </div>
+      )}
+      {cart.map((carts) => (
+        <Grid item md={3} key={carts.id}>
+          <Card className={classes.root}>
+            <CardHeader
+              avatar={
+                <Avatar className={classes.avatar}>
+                  {carts.name[0].toUpperCase()}
+                </Avatar>
+              }
+              title={carts.name}
+              action={
+                <IconButton onClick={() => handleDeleate(carts.id)}>
+                  <DeleteOutline />
+                </IconButton>
+              }
+            />
+            <CardContent>
+              <Typography component="i">From</Typography>
+              <Typography variant="h4">
+                {carts.start} <ArrowRightAlt /> {carts.stop}
+              </Typography>
+              <Typography color="primary">{carts.date}</Typography>
+              <Typography>{carts.time} Hours</Typography>
+              <Typography className={classes.priceName}>
+                ₦{carts.price}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" className={classes.bookNowButton}>
+                Update
+              </Button>
+              <Link to={"/vieworder"}>
+                <Button size="small">
+                  orders 10
+                  <ArrowRightSharp />
+                </Button>
+              </Link>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </>
+  );
 }
 
 export default ACard;
--- src/user/HomeCompanents/FlightCart.jsx
+++ src/user/HomeCompanents/FlightCart.jsx
@@ -7,9 +7,9 @@ import {
   CardHeader,
   Grid,
   makeStyles,
-  Typography,
 } from "@material-ui/core";
 import { ArrowRightAlt } from "@mui/icons-material";
+import { Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -30,41 +30,63 @@ const useStyles = makeStyles({
 
 function FlightCart() {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
   const [cart, setCart] = useState([]);
+  const [ispending, setpending] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:8000/cart")
       .then((res) => res.json())
-      .then((data) => setCart(data));
+      .then((data) => {
+        setCart(data);
+       setpending(false);
+      });
   }, []);
 
-  return cart.map((carts) => (
-    <Grid item md={3}>
-      <Card className={classes.root} key={carts.id}>
-        <CardHeader
-          avatar={<Avatar className={classes.avatar}>C</Avatar>}
-          title={carts.name}
-        />
-        <CardContent>
-          <Typography component="i">from</Typography>
-          <Typography variant="h4">
-            {carts.start} <ArrowRightAlt /> {carts.stop}
+  return (
+    <>
+      {ispending && (
+        <div style={{
+          width:"100%"
+        }} >
+          <Typography variant="h1" color="black" textAlign={"center"}>
+            Loading...
           </Typography>
-          <Typography color="primary">{carts.date}</Typography>
-          <Typography>{carts.time} hours</Typography>
-        </CardContent>
-        <CardActions>
-          <Link to={"/bookFlight"}>
-            <Button size="small" className={classes.bookNowButton}>
-              Book now
-            </Button>
-          </Link>
-          <Typography className={classes.priceName}>₦{carts.price}</Typography>
-        </CardActions>
-      </Card>
-    </Grid>
-  ));
+        </div>
+      )}
+      {cart.map((carts) => (
+        <Grid item md={3}>
+          <Card className={classes.root} key={carts.id}>
+            <CardHeader
+              avatar={
+                <Avatar className={classes.avatar}>
+                  {carts.name[0].toUpperCase()}
+                </Avatar>
+              }
+              title={carts.name}
+            />
+            <CardContent>
+              <Typography component="i">from</Typography>
+              <Typography variant="h4">
+                {carts.start} <ArrowRightAlt /> {carts.stop}
+              </Typography>
+              <Typography color="primary">{carts.date}</Typography>
+              <Typography>{carts.time} hours</Typography>
+            </CardContent>
+            <CardActions>
+              <Link to={"/bookFlight"}>
+                <Button size="small" className={classes.bookNowButton}>
+                  Book now
+                </Button>
+              </Link>
+              <Typography className={classes.priceName}>
+                ₦{carts.price}
+              </Typography>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </>
+  );
 }
 
 export default FlightCart;
